e2e8885ddb36d9031f30a100f64d11a1d621caee
RU <110257560+BnanZ0@users.noreply.github.com>
2026-05-24 18:41:37 +0800

    fix test

--- tests/TestLockpick.py
+++ tests/TestLockpick.py
@@ -17,21 +17,21 @@ class TestOcrLv(TaskTestCase):
         # Create a BattleReport object
         self.set_image('tests/images/lock_pick/01.png')
         start_time = time.time()
-        result = self.task.find_lock_pick()
+        result = self.task.is_lock_pick_active()
         self.logger.info(f'test_lock_pick_1: {result} {time.time() - start_time:.2f}s')
         self.assertEqual(result, True)
 
     def test_lock_pick_2(self):
         self.set_image('tests/images/lock_pick/02.png')
         start_time = time.time()
-        result = self.task.find_lock_pick()
+        result = self.task.is_lock_pick_active()
         self.logger.info(f'test_lock_pick_2: {result} {time.time() - start_time:.2f}s')
         self.assertEqual(result, True)
 
     def test_lock_pick_3(self):
         self.set_image('tests/images/lock_pick/03.png')
         start_time = time.time()
-        result = self.task.find_lock_pick()
+        result = self.task.is_lock_pick_active()
         self.logger.info(f'test_lock_pick_3: {result} {time.time() - start_time:.2f}s')
         self.assertEqual(result, True)
 
